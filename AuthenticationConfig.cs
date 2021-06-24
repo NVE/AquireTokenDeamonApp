86297e1d70789043dde41a547708ea9f0eafbffc
wartiainen <awa@nve.no>
2021-06-24 09:52:24 +0200

    Add files via upload

---
+++ AuthenticationConfig.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Security;
+
+namespace TestDeamonApp
+{
+	/// <summary>
+	/// Description of the configuration of an AzureAD public client application (desktop/mobile application). This should
+	/// match the application registration done in the Azure portal
+	/// </summary>
+	public class AuthenticationConfig
+    {
+        /// <summary>
+        /// Guid used by the application to uniquely identify itself to Azure AD
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// URL of the authority
+        /// </summary>
+        public string Authority { get; set; }
+
+        /// <summary>
+        /// Web Api base URL
+        /// </summary>
+        public string ApiBaseAddress { get; set; }
+
+        /// <summary>
+        /// Web Api scope. With client credentials flows, the scopes is ALWAYS of the shape "resource/.default"
+        /// </summary>
+        public string Scope { get; set; }
+
+        /// <summary>
+        /// Regobs App token, a GUID, needed to be able to call the API
+        /// </summary>
+        public string RegObsApptoken { get; set; }
+
+        /// <summary>
+        /// Example regObs API endpoint to call. e.g Account/GetObserver, will get info about the logged in observer
+        /// </summary>
+        public string RegObsActionToCall { get; set; }
+
+        /// <summary>
+        /// NVE-Account login email address
+        /// </summary>
+        public string NVEAccountEmail { get; set; }
+
+        /// <summary>
+        /// NVE-Account password
+        /// </summary>
+        public string NVEAccountPassword { get; set; }
+
+
+        public SecureString SecretPassword
+        {
+            get
+            {
+                var passwordText = new SecureString();
+                foreach (var c in NVEAccountPassword)
+                {
+                    passwordText.AppendChar(c);
+                }
+                return passwordText;
+            }
+        }
+
+        /// <summary>
+        /// Reads the configuration from a json file
+        /// </summary>
+        /// <param name="path">Path to the configuration json file</param>
+        /// <returns>AuthenticationConfig read from the json file</returns>
+        public static AuthenticationConfig ReadFromJsonFile(string path)
+        {
+            IConfigurationRoot Configuration;
+
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(path);
+
+            Configuration = builder.Build();
+            return Configuration.Get<AuthenticationConfig>();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestDeamonApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            try
+            {
+                RunAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static async Task RunAsync()
+        {
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+                        
+            IPublicClientApplication app;
+
+            app = PublicClientApplicationBuilder.Create(config.ClientId)
+                .WithB2CAuthority(config.Authority)
+                .Build();
+
+            string[] scopes = new string[] { config.Scope };
+
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenByUsernamePassword(scopes, config.NVEAccountEmail, config.SecretPassword).ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Token acquired from {config.Authority} \n");
+                Console.ResetColor();
+            }
+            catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Scope provided is not supported");
+                Console.ResetColor();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error acquiring token: {e.Message}");
+                Console.ResetColor();
+            }
+
+            if (result != null)
+            {
+                var httpClient = new HttpClient();
+                var apiCaller = new ProtectedApiCallHelper(httpClient);
+                await apiCaller.CallWebApiAndProcessResultASync(config, $"{config.ApiBaseAddress}/{config.RegObsActionToCall}", result.AccessToken, Display);
+            }
+        }
+
+        private static void Display(string result)
+        {
+            Console.WriteLine($"Web Api result: {result}");            
+        }
+    }
+}
